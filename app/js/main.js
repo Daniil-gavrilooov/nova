fc01f35bd1783087d617771680714c9564254d41
Daniil-gavrilooov <daniil..gavrilooov@gmail.com>
2022-08-10 18:23:48 +0300

    added style section (project)

--- app/js/main.js
+++ app/js/main.js
@@ -6,3 +6,11 @@ function burgerMenu () {
     burgerBtn.classList.toggle('header__burger-btn--active')
     burgerMenu.classList.toggle('header__burger-menu--active')
 }
+
+$(function(){
+    $('.project__slider').slick({
+        slidesToShow: 1,
+        prevArrow: '<img class="prev__arrow" src="images/project_slider-prevArrow.svg" alt="">',
+        nextArrow: '<img class="next__arrow" src="images/project_slider-nextArrow.svg" alt="">'
+    });
+  });
